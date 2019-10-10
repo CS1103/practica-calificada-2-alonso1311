59a7a43100853664cbc7a0a4a6b2a7bfb0834179
alonso1311 <alonso.barrios@utec.edu.pe>
2019-10-10 18:27:35 -0500

    PC Terminada

--- heap.h
+++ heap.h
@@ -5,4 +5,89 @@
 #ifndef CS1103_PC2_201902_HEAP_H
 #define CS1103_PC2_201902_HEAP_H
 
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+template <typename T>
+class heap {
+private:
+    vector<T> v;
+    T valores;
+
+public:
+    heap() {}
+
+    T get_max() {
+        return *begin(v);
+    }
+
+    void insert(T item) {
+        v.push_back(item);
+        sort(begin(v), end(v), greater<T>());
+    }
+
+    template <typename... T1>
+    auto insert(T1 ... items) {
+        for(auto& i : {items...}) {
+            v.push_back(i);
+        }
+        sort(begin(v), end(v), greater<T>());
+    }
+
+    void clean() {
+        v.clear();
+    }
+
+    friend ostream& operator<< (ostream& out, const heap<T> heap) {
+        for(auto& i : heap.v)
+            out << i << " ";
+
+        return out;
+    }
+
+    friend istream& operator>> (istream& in, heap<T>& heap) {
+        cout << "\nIngrese elemento: ";
+        in >> heap.valores;
+        heap.insert(heap.valores);
+
+        return in;
+    }
+};
+
+template <typename T>
+void load_from(const string& nombreArchivo, heap<T>& h) {
+    h.clean();
+
+    fstream file;
+    file.open(nombreArchivo, ios::in);
+    string fila;
+
+    if (file.is_open()) {
+        while (getline(file, fila)) {
+            stringstream ss(fila);
+            string dato;
+            getline(ss, dato);
+            h.insert(stoi(dato));
+        }
+        file.close();
+    }
+    else
+        cout << "No se abrio el archivo" << endl;
+}
+
+template <typename T, typename... T1>
+void insert (heap<T>& hp, T first, T1... items) {
+    hp.insert(first);
+    for(auto& i : {items...}) {
+        hp.insert(i);
+    }
+}
+
+
+
 #endif //CS1103_PC2_201902_HEAP_H
--- main.cpp
+++ main.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
-
+#include <string>
 #include "heap.h"
+
 using namespace std;
 
 int main()
 {
     heap<int> h;
-    load_from("data.txt", h);
-    cout << "heap" << endl;
+    load_from(string("data.txt"), h);
+    cout << "heap con lectura" << endl;
     cout << h;
     h.insert(10);
     h.insert(20);
     h.insert(40);
     h.insert(50);
     h.insert(10);
-    cout << "heap" << endl;
+    cout << "\nheap insert 1 elementos:" << endl;
     cout << h;
-    cout << "heap" << endl;
+    h.insert(20, 30, 40);
+    cout << "\nheap insert n elementos:" << endl;
     cout << h;
+
+    cout << "\nMax:" << h.get_max();
+
     cin >> h;
-    cout << "heap" << endl;
+    cout << "\nheap cin:" << endl;
     cout << h;
+
     insert(h, 100, 200);
-    cout << "heap" << endl;
+    cout << "\nheap insert fuera de la clase" << endl;
+    cout << h;
+
+    h.clean();
+    cout << "\nheap clean" << endl;
     cout << h;
     load_from("data.txt", h);
+    cout << "heap con lectura 2" << endl;
     cout << h;
     return 0;
 }
+
+
